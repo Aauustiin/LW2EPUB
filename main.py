aedf55a2aed4e314d91a03643464f54bb4c0a9dd
Aauustiin <longaustin2001@gmail.com>
2024-06-16 18:37:06 +0100

    works well for single post

--- main.py
+++ main.py
@@ -1,57 +1,69 @@
 import argparse
 from urllib.request import urlopen
 from ebooklib import epub
+from bs4 import BeautifulSoup
 
 parser = argparse.ArgumentParser(
     description="",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
 parser.add_argument("--url")
+parser.add_argument("--type", default="post")
 
-def main(args):
-    page = urlopen(args.url)
+def createChapterFromPostURL(postUrl):
+    page = urlopen(postUrl)
     html_bytes = page.read()
     html = html_bytes.decode("utf-8")
-    print(html)
+    soup = BeautifulSoup(html, 'html.parser')
+    content = soup.find('div', id="postBody")
+    title = soup.find('h1', class_="Typography-root Typography-display3 PostsPageTitle-root")
+    title = title.get_text()
+    author = soup.find('a', class_="UsersNameDisplay-noColor")
+    author = author.get_text()
+
+    chapter = epub.EpubHtml(title=title,
+                    file_name=title + '.xhtml',
+                    lang='en',
+                    uid=title)
+    
+    chapter.set_content("<h1>" + str(title) + "</h1>" + str(content))
+    return chapter, author
+
+def createEbookFromPost(url):
     book = epub.EpubBook()
-    book.set_identifier('sample123456')
-    book.set_title('Sample book')
+
+    chapter, author = createChapterFromPostURL(url)
+    book.add_item(chapter)
+
+    book.set_title(chapter.title)
+    book.set_identifier(chapter.title)
+    book.add_author(author)
     book.set_language('en')
+    description = "The post \"" + chapter.title + "\", formatted as an .epub."
+    book.add_metadata('DC', 'description', description)
 
-    book.add_author('Aleksandar Erkalovic')
-    book.add_metadata('DC', 'description', 'This is description for my book')
-    book.add_metadata(None, 'meta', '', {'name': 'key', 'content': 'value'})
-
-    # intro chapter
-    c1 = epub.EpubHtml(title='Introduction',
-                    file_name='intro.xhtml',
-                    lang='en')
-    c1.set_content(u'<html><body><h1>Introduction</h1><p>Introduction paragraph.</p></body></html>')
-
-    # about chapter
-    c2 = epub.EpubHtml(title='About this book',
-                    file_name='about.xhtml')
-    c2.set_content('<h1>About this book</h1><p>This is a book.</p>')
-    book.add_item(c1)
-    book.add_item(c2)
-    style = 'body { font-family: Times, Times New Roman, serif; }'
-
-    nav_css = epub.EpubItem(uid="style_nav",
-                            file_name="style/nav.css",
-                            media_type="text/css",
-                            content=style)
-    book.add_item(nav_css)
-    book.toc = (epub.Link('intro.xhtml', 'Introduction', 'intro'),
-              (
-                epub.Section('Languages'),
-                (c1, c2)
-              )
-            )
-    book.spine = ['nav', c1, c2]
+    book.toc = (epub.Link(chapter.file_name, chapter.title, chapter.title),)
+    book.spine = [chapter]
     book.add_item(epub.EpubNcx())
     book.add_item(epub.EpubNav())
-    epub.write_epub('test.epub', book)
-    print("Created ebook!")
+
+    epub.write_epub(chapter.title + '.epub', book)
+    print("Successfully created ebook!")
+    
+
+def createEbookFromSequence(url):
+    print("Not yet implemented.")
+    # Scrape URLs for all the posts
+    # Create chapters for each of the posts
+    # Add authors
+    # Scrape title
+    # Set title
+
+def main(args):
+    if args.type == "post":
+        createEbookFromPost(args.url)
+    elif args.type == "sequence":
+        createEbookFromSequence(args.url)
 
 if __name__ == "__main__":
     main(parser.parse_args())
